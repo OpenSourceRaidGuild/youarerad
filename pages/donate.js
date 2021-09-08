45686de626cccd5ba090be449f1f9abf08265cf8
Jedibezy <Jdocton@gmail.com>
2021-09-07 19:37:06 -0700

    partner page fix

--- pages/donate.js
+++ pages/donate.js
@@ -146,30 +146,3 @@ export default function Donate() {
     </Layout>
   )
 }
-
-const Partners = [
-  {
-    name: 'Jagex',
-    logo: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1629765022/Logos/Jagexlogo.png.png',
-    alt: 'Jagex, makers of Runescape, support Rise Above The Disorder.',
-    link: 'https://www.jagex.com/en-GB/charitable-giving',
-  },
-  {
-    name: 'Electronic Arts',
-    logo: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1629766474/Logos/EAlogo.png.png',
-    alt: 'Electronic Arts supports Rise Above The Disorder.',
-    link: 'https://www.ea.com/compete/wellness',
-  },
-  {
-    name: 'G4.tv',
-    logo: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1629765666/Logos/G4logo.png.png',
-    alt: 'G4.tv supports Rise Above The Disorder.',
-    link: 'https://g4tv.com/',
-  },
-  {
-    name: 'Columbia Records',
-    logo: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1629765876/Logos/ColumbiaRecordslogo.jpg.jpg',
-    alt: 'Columbia Records supports Rise Above The Disorder.',
-    link: 'http://www.columbiarecords.com/',
-  },
-]
