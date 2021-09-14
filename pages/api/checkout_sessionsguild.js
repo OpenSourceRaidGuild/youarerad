64d44b4add370f8744bd6a90510d68f611eb36ca
Jedibezy <Jdocton@gmail.com>
2021-09-14 15:41:29 -0700

    axios guild test

--- pages/api/checkout_sessionsguild.js
+++ pages/api/checkout_sessionsguild.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
           },
         ],
 
-        success_url: `${req.headers.origin}/Success?session_id={CHECKOUT_SESSION_ID}`,
+        success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/`,
       }
       const checkoutSession = await stripe.checkout.sessions.create(params)
