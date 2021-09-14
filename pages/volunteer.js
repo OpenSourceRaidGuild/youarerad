f44d337b62b5dfe8489cb80018fc29873e38fcba
Jedibezy <Jdocton@gmail.com>
2021-09-13 18:24:29 -0700

    ios focus fix

--- pages/volunteer.js
+++ pages/volunteer.js
@@ -116,16 +116,16 @@ const Volunteer = () => {
                 <Tab.Group as="div" className="mt-4">
                   <div className="flex overflow-x-auto sm:mx-0">
                     <div className="flex-auto border-b border-gray-200 sm:px-0">
-                      <Tab.List className="flex justify-between sm:flex sm:justify-start sm:space-x-10">
+                      <Tab.List className="flex justify-between sm:flex sm:justify-start sm:space-x-10 focus:outline-none">
                         {tabs.map((tab) => (
                           <Tab
                             key={tab.name}
                             className={({ selected }) =>
                               classNames(
                                 selected
-                                  ? 'border-indigo-500 text-indigo-600 focus:ring-transparent'
+                                  ? 'border-indigo-500 text-indigo-600 focus:outline-none'
                                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                                'whitespace-nowrap py-6 border-b-2 font-medium text-md focus:ring-transparent'
+                                'whitespace-nowrap py-6 border-b-2 font-medium text-md focus:outline-none'
                               )
                             }
                           >
