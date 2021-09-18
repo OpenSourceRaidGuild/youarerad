4290dedd2accbd6eaef17b6c6222698618812b22
Jedibezy <Jdocton@gmail.com>
2021-09-17 21:25:39 -0700

    langscript

--- pages/_document.js
+++ pages/_document.js
@@ -10,17 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="alternate" hrefLang="en" href="https://www.youarerad.org" />
-          <link rel="alternate" hrefLang="fr" href="https://fr.youarerad.org" />
-          <link rel="alternate" hrefLang="de" href="https://de.youarerad.org" />
-          <link rel="alternate" hrefLang="ja" href="https://ja.youarerad.org" />
-          <link rel="alternate" hrefLang="ko" href="https://ko.youarerad.org" />
-          <link rel="alternate" hrefLang="pt" href="https://pt-br.youarerad.org" />
-          <link rel="alternate" hrefLang="ru" href="https://ru.youarerad.org" />
-          <link rel="alternate" hrefLang="zh" href="https://zh.youarerad.org" />
-          <link rel="alternate" hrefLang="es" href="https://es.youarerad.org" />
-          <link rel="alternate" hrefLang="th" href="https://th.youarerad.org" />
-          <link rel="alternate" hrefLang="vi" href="https://vi.youarerad.org" />
           <script async type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
           <script
             dangerouslySetInnerHTML={{
