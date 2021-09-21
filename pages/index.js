3bd61c9859cd1a56433c89599cc5eb97ef37a7e3
Jedibezy <Jdocton@gmail.com>
2021-09-21 00:54:12 -0700

    height adjust main title

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         pageLink="/"
         description="We are a non-profit covering the cost of mental health care for thousands of people around the world."
       >
-        <section className="h-[80vh] pt-0  lg:pt-40">
+        <section className="h-[100vh] pt-0  lg:pt-40">
           <h1 className="display">
             We are a nonprofit covering the cost of mental health care for{' '}
             <span className="text-transparent bg-gradient-to-r from-pink-400 to-blue-400 bg-clip-text">
