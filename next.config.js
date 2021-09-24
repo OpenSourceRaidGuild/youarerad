375e2f8aecc9ebd83ce5f01a57aa3a4ecc1fadf1
Jedibezy <Jdocton@gmail.com>
2021-09-23 20:21:39 -0700

    x-xss protection added

--- next.config.js
+++ next.config.js
@@ -1,51 +1,15 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({
-  experimental: { esmExternals: true },
-  reactStrictMode: true,
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  images: {
-    domains: ['res.cloudinary.com', 'pbs.twimg.com', 'abs.twimg.com'],
-  },
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ]
-  },
-})
-// https://securityheaders.com
-const ContentSecurityPolicy = `
-  default-src 'self';
-  frame-src https://js.stripe.com https://hooks.stripe.com;
-  script-src 'self' www.google-analytics.com ajax.googleapis.com https://js.stripe.com;
-  child-src *.youtube.com *.google.com *.twitter.com;
-  style-src 'self' 'unsafe-inline' *.googleapis.com;
-  media-src *.cloudinary.com;
-  img-src 'self' *.cloudinary.com;
-  connect-src *;
-  font-src 'self';
-`
-
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
     value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
@@ -79,3 +43,42 @@ const securityHeaders = [
     value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
   },
 ]
+module.exports = withBundleAnalyzer({
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+  experimental: { esmExternals: true },
+  reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  images: {
+    domains: ['res.cloudinary.com', 'pbs.twimg.com', 'abs.twimg.com'],
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+})
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  frame-src https://js.stripe.com https://hooks.stripe.com;
+  script-src 'self' www.google-analytics.com ajax.googleapis.com https://js.stripe.com;
+  child-src *.youtube.com *.google.com *.twitter.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  media-src *.cloudinary.com;
+  img-src 'self' *.cloudinary.com;
+  connect-src *;
+  font-src 'self';
+`
