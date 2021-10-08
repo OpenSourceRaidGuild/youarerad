8f67a8d677af6f7cf77ed13e5cf584fca4333c3a
Jedibezy <Jdocton@gmail.com>
2021-10-08 12:58:23 -0700

    Section gradients

--- components/Sectionheader.js
+++ components/Sectionheader.js
@@ -2,7 +2,7 @@ export default function Sectionheader({ subheader, headerone, headertwo, bodytex
   return (
     <div className="max-w-4xl">
       <h5>{subheader}</h5>
-      <h2 className="block text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-pink-400">
+      <h2 className="text-rad">
         <span className={color}>
           {headerone} {''}
         </span>
