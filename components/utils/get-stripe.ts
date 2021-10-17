072ca7dc0bb316366d6141f408e584b16cd6af83
Michael Peyper <mpeyper7@gmail.com>
2021-10-17 20:32:44 +1100

    convert get-stripe to typescript

--- components/utils/get-stripe.ts
+++ components/utils/get-stripe.ts
@@ -1,6 +1,10 @@
 import { loadStripe } from '@stripe/stripe-js'
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+declare const stripe: {
+  loadStripe: typeof loadStripe
+};
+
+let stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 const getStripe = () => {
   if (!stripePromise) {
     stripePromise = stripe.loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
