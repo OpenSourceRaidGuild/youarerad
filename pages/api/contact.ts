84541c04820e497e4e87c3f53a3a85cfa8ea08aa
JacobMGEvans <jacobmgevans@gmail.com>
2021-10-17 19:53:57 -0500

    PersonalDetails, Contact, Partnership API, volunteer API

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,10 +1,12 @@
 import { supabase } from '../../components/utils/supabaseClient'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function sendContact(req, res) {
+
+export default async function sendContact(req: NextApiRequest, res: NextApiResponse) {
   const { email, department, name, message } = req.body
-  let { user, error } = await supabase
+  let { data, error } = await supabase
     .from('contactrad')
     .insert({ email: email, department: department, name: name, message: message })
   if (error) return res.status(401).json({ error: error.message })
-  return res.status(200).json({ user: user })
+  return res.status(200).json({ user: data })
 }
